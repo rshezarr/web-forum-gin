1f1cf8ff8cc6ffed2ba730eca032c2976e8ffd09
rshezarr <rshezarr@gmail.com>
2023-01-05 00:37:53 +0600

    update: main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"forum/internal/app"
 
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		logrus.Fatal(err)
+	}
+
+	app.Run()
+}
+
+func initConfig() error {
+	var configPath = flag.String("config-path", "configs/", "path to config file")
+
+	flag.Parse()
+
+	viper.SetConfigName("forum")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(*configPath)
+
+	return viper.ReadInConfig()
 }
