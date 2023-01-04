48f6915ee42fc5b78b623771666c336c04a5af1a
rshezarr <rshezarr@gmail.com>
2023-01-05 00:29:58 +0600

    init: configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,42 @@
 package config
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type (
+	Config struct {
+		API      API      `mapstructure:"api"`
+		Database Database `mapstructure:"database"`
+	}
+
+	API struct {
+		Host           string `mapstructure:"host"`
+		Addr           string `mapstructure:"addr"`
+		MaxHeaderBytes int    `mapstructure:"maxHeaderByte"`
+		ReadTimeout    int    `mapstructure:"readTimeout"`
+		WriteTimeout   int    `mapstructure:"writeTimeout"`
+	}
+
+	Database struct {
+		Driver      string `mapstructure:"driver"`
+		DBName      string `mapstructure:"dbname"`
+		DatabaseURL string `mapstructure:"databaseUrl"`
+		SchemePath  string `mapstructure:"schemesPath"`
+	}
+)
+
+func NewConfig() (*Config, error) {
+	logrus.Info("Configs are initializing...")
+
+	var config *Config
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return &Config{}, err
+	}
+
+	logrus.Info("Configs are initialized")
+
+	return config, nil
+}
