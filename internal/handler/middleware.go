ba9394c0629b1d012667301959dec8cb6d99ab41
rshezarr <rshezarr@gmail.com>
2023-01-05 05:58:06 +0600

    update: middleware

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,1 +1,27 @@
 package handler
+
+import (
+	"context"
+	"net/http"
+)
+
+const ctxKeyUser ctxKey = iota
+
+type ctxKey int8
+
+func (h *Handler) authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("session_cookie")
+		if err != nil {
+			return
+		}
+
+		userID, err := h.service.ParseToken(cookie.Value)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyUser, userID)))
+	}
+}
