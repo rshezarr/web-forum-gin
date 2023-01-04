a3f0b8607d3764f0d15aa00dee3035ead74314c2
rshezarr <rshezarr@gmail.com>
2023-01-05 00:28:27 +0600

    init: database conenction layer

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module forum
 
 go 1.19
 
-require github.com/jmoiron/sqlx v1.3.5
+require (
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/pressly/goose/v3 v3.7.0
+	github.com/sirupsen/logrus v1.9.0
+)
+
+require (
+	github.com/lib/pq v1.10.7 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -1,1 +1,28 @@
 package database
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/pressly/goose/v3"
+	"github.com/sirupsen/logrus"
+)
+
+func ConnectDB(driver, databaseURL, schemePath string) (*sqlx.DB, error) {
+	logrus.Info("Connecting to database...")
+
+	db, err := sqlx.Connect(driver, databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("error while database connection: %w", err)
+	}
+
+	logrus.Info("Creating tables...")
+
+	if err := goose.Up(db.DB, schemePath, goose.WithNoVersioning()); err != nil {
+		logrus.Fatalf("migration: %s", err.Error())
+	}
+
+	logrus.Info("Tables are created and successful conenction")
+
+	return db, nil
+}
