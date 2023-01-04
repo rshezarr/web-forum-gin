67b4daae04bbdde82bdba0a7f899d00cf69953ae
rshezarr <rshezarr@gmail.com>
2023-01-05 05:09:17 +0600

    update: user model

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type User struct {
-	ID             int       `json:"id"`
+	ID             int       `json:"user_id"`
 	Email          string    `json:"user_email"`
 	Username       string    `json:"user_username"`
 	Password       string    `json:"user_password"`
