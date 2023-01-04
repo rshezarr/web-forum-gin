e84b5b3460ff62ec7a9cf0df8171805367df37ac
rshezarr <rshezarr@gmail.com>
2023-01-05 00:22:27 +0600

    init: handler layer

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"forum/internal/service"
+)
+
+type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler {
+	return &Handler{service: service}
+}
