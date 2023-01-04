e1f3edc86d9957b3e3f2e9a5fcfae1127a37695d
rshezarr <rshezarr@gmail.com>
2023-01-05 04:53:30 +0600

    update: sign in handler

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -40,5 +40,31 @@ func (h *Handler) signUp(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) signIn(w http.ResponseWriter, r *http.Request) {
+	var user model.User
 
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		logrus.Errorf("sign in: new decode - %s", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.Unmarshal(data, &user); err != nil {
+		logrus.Errorf("sign up: unmarshal - %s", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	token, err := h.service.GenerateToken(user.Email, user.Password)
+	if err != nil {
+		logrus.Errorf("sign up: unmarshal - %s", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(&token); err != nil {
+		logrus.Errorf("sign up: response id - %s", err.Error())
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
