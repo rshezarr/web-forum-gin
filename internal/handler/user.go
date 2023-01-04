d96b28b0587e08db7d6cad383f9c0f666ebb46ba
rshezarr <rshezarr@gmail.com>
2023-01-05 05:07:35 +0600

    fix: sign in handlers errors

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -50,20 +50,20 @@ func (h *Handler) signIn(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := json.Unmarshal(data, &user); err != nil {
-		logrus.Errorf("sign up: unmarshal - %s", err.Error())
+		logrus.Errorf("sign in: unmarshal - %s", err.Error())
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	token, err := h.service.GenerateToken(user.Email, user.Password)
 	if err != nil {
-		logrus.Errorf("sign up: unmarshal - %s", err.Error())
+		logrus.Errorf("sign in: generate token - %s", err.Error())
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
 	if err := json.NewEncoder(w).Encode(&token); err != nil {
-		logrus.Errorf("sign up: response id - %s", err.Error())
+		logrus.Errorf("sign in: response id - %s", err.Error())
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
