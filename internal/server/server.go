1c4b3d02c52e22bde349f6de5e909e28fea9632b
rshezarr <rshezarr@gmail.com>
2023-01-05 00:30:40 +0600

    init: server layer

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,38 @@
 package server
+
+import (
+	"context"
+	"forum/internal/config"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Server struct {
+	server *http.Server
+}
+
+func NewServer(cfg *config.Config, router *http.ServeMux) *Server {
+	return &Server{
+		server: &http.Server{
+			Addr:           cfg.API.Addr,
+			Handler:        router,
+			MaxHeaderBytes: cfg.API.MaxHeaderBytes << 20,
+			ReadTimeout:    time.Duration(cfg.API.ReadTimeout * int(time.Second)),
+			WriteTimeout:   time.Duration(cfg.API.WriteTimeout * int(time.Second)),
+		},
+	}
+}
+
+func (s *Server) Run() error {
+	logrus.Infof("Server is started at port http://localhost%s", s.server.Addr)
+
+	return s.server.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	logrus.Info("Server shutting down...")
+
+	return s.server.Shutdown(ctx)
+}
