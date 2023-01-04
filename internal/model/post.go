3d9ca7cc48e1d234f5900420f5343716fe7c962f
rshezarr <rshezarr@gmail.com>
2023-01-05 05:11:12 +0600

    update: post model

--- internal/model/post.go
+++ internal/model/post.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Post struct {
-	ID           int `json:"post_id"`
-	UserID       int
+	ID           int       `json:"post_id"`
+	UserID       int       `json:"post_user_id"`
 	Title        string    `json:"post_title"`
 	Content      string    `json:"post_content"`
 	CreationTime time.Time `json:"post_createdAt"`
