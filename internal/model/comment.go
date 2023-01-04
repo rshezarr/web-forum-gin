f849a8aa2a60b57acd50eb1f2b39c9cadb8f708e
rshezarr <rshezarr@gmail.com>
2023-01-05 05:10:41 +0600

    update: coment model

--- internal/model/comment.go
+++ internal/model/comment.go
@@ -1,10 +1,10 @@
 package model
 
 type Comment struct {
-	ID       int
+	ID       int    `json:"comment_id"`
 	UserID   int    `json:"comment_user_id"`
 	PostID   int    `json:"comment_post_id"`
 	Content  string `json:"comment-content"`
-	Likes    int
-	Dislikes int
+	Likes    int    `json:"comment_likes"`
+	Dislikes int    `json:"comment_dislikes"`
 }
