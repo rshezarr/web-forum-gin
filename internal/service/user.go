3f6600e275a6d385b0af36f633ea3893d245a84f
rshezarr <rshezarr@gmail.com>
2023-01-05 04:48:56 +0600

    update: get user service

--- internal/service/user.go
+++ internal/service/user.go
@@ -1,13 +1,28 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"forum/internal/model"
 	"forum/internal/repository"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
+const (
+	salt       = "hjqrhjqw124617ajfhajs"
+	signingKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 type User interface {
 	CreateUser(user model.User) (int, error)
-	GenerateToken(username, password string) (model.User, error)
+	GenerateToken(username, password string) (string, error)
 	ParseToken(token string) (model.User, error)
 	DeleteToken(token string) error
 }
@@ -22,6 +37,12 @@ func NewUser(repo repository.User) *UserService {
 	}
 }
 
+func generateHashPassword(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
+
 func (s *UserService) CreateUser(user model.User) (int, error) {
 	id, err := s.repo.CreateUser(user)
 	if err != nil {
@@ -31,8 +52,26 @@ func (s *UserService) CreateUser(user model.User) (int, error) {
 	return id, nil
 }
 
-func (s *UserService) GenerateToken(username string, password string) (model.User, error) {
-	panic("not implemented") // TODO: Implement
+func (s *UserService) GenerateToken(email string, password string) (string, error) {
+	user, err := s.repo.GetUser(email, generateHashPassword(password))
+	if err != nil {
+		return "", fmt.Errorf("service: generate token: %w", err)
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.ID,
+	})
+
+	user.Token, err = token.SignedString([]byte(signingKey))
+	if err != nil {
+		return "", err
+	}
+
+	return user.Token, nil
 }
 
 func (s *UserService) ParseToken(token string) (model.User, error) {
