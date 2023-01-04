2565ec0c4f96d4f0eca80b081fbeeebd0d41dbb9
rshezarr <rshezarr@gmail.com>
2023-01-05 00:20:02 +0600

    init: repository layer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module forum
 
 go 1.19
+
+require github.com/jmoiron/sqlx v1.3.5
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,10 @@
 package repository
+
+import "github.com/jmoiron/sqlx"
+
+type Repository struct {
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{}
+}
