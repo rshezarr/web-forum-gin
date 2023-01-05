395e846db037aab46d38734a4f859d79629dada4
rshezarr <rshezarr@gmail.com>
2023-01-05 07:39:35 +0600

    update: post model

--- internal/model/post.go
+++ internal/model/post.go
@@ -8,7 +8,6 @@ type Post struct {
 	Title        string    `json:"post_title"`
 	Content      string    `json:"post_content"`
 	CreationTime time.Time `json:"post_createdAt"`
-	Category     []string  `json:"post_categories"`
 	Likes        int       `json:"post_likes"`
 	Dislikes     int       `json:"post_dislikes"`
 }
