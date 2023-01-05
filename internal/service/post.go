951eef6c05dbadec7e4611e35118e31d0760332f
rshezarr <rshezarr@gmail.com>
2023-01-05 06:53:21 +0600

    update: service create post

--- internal/service/post.go
+++ internal/service/post.go
@@ -1,8 +1,19 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"forum/internal/model"
 	"forum/internal/repository"
+)
+
+var (
+	ErrInvalidPostTitle   = errors.New("invalid post title characters")
+	ErrInvalidPostContent = errors.New("invalid post content characters")
+	ErrPostTitleLen       = errors.New("title length out of range")
+	ErrPostContentLen     = errors.New("content length out of range")
+	ErrImageSize          = errors.New("image size bigger than 20MB")
+	ErrImageType          = errors.New("invalid image type")
 )
 
 type Post interface {
@@ -21,8 +32,37 @@ func NewPost(repo repository.Post) *PostService {
 	}
 }
 
+func checkPost(post model.Post) error {
+	if len(post.Title) > 100 {
+		return fmt.Errorf("service: Create Post: check post: %w", ErrPostTitleLen)
+	}
+
+	if len(post.Content) > 1500 {
+		return fmt.Errorf("service: Create Post: check post: %w", ErrPostContentLen)
+	}
+
+	if post.Title == "" {
+		return fmt.Errorf("service: Create Post: check post: %w", ErrInvalidPostTitle)
+	}
+
+	if post.Content == "" {
+		return fmt.Errorf("service: Create Post: check post: %w", ErrInvalidPostContent)
+	}
+
+	return nil
+}
+
 func (s *PostService) CreatePost(post model.Post) (int, error) {
-	panic("not implemented") // TODO: Implement
+	if err := checkPost(post); err != nil {
+		return 0, err
+	}
+
+	id, err := s.repo.CreatePost(post)
+	if err != nil {
+		return 0, fmt.Errorf("service: create post: %w", err)
+	}
+
+	return id, nil
 }
 
 func (s *PostService) GetAllPosts() ([]model.Post, error) {
