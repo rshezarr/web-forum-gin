db808ccfed2e57dff07bd2192bc691df197596a4
rshezarr <rshezarr@gmail.com>
2023-01-05 07:02:30 +0600

    init: post handlers

--- internal/handler/post.go
+++ internal/handler/post.go
@@ -5,3 +5,19 @@ import "net/http"
 func (h *Handler) createPost(w http.ResponseWriter, r *http.Request) {
 
 }
+
+func (h *Handler) editPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) deletePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) likePost(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *Handler) dislikePost(w http.ResponseWriter, r *http.Request) {
+
+}
