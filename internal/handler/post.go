2122a72877c9a8326e655f9e43623ecb13f47b38
rshezarr <rshezarr@gmail.com>
2023-01-05 07:46:15 +0600

    update: posts handler

--- internal/handler/post.go
+++ internal/handler/post.go
@@ -1,9 +1,25 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+)
 
 func (h *Handler) posts(w http.ResponseWriter, r *http.Request) {
+	posts, err := h.service.Post.GetAllPosts()
+	if err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 
+	if err := json.NewEncoder(w).Encode(&posts); err != nil {
+		logrus.Error(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
 
 func (h *Handler) createPost(w http.ResponseWriter, r *http.Request) {
