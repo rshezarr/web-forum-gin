67861e165fb00d6368d90c8ce156b3b06ca6f7c8
rshezarr <rshezarr@gmail.ru>
2023-01-05 19:57:12 +0600

    add: like post repo

--- internal/repository/votePost.go
+++ internal/repository/votePost.go
@@ -1,9 +1,12 @@
 package repository
 
 import (
+	"context"
+	"fmt"
 	"forum/internal/model"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
 )
 
 type VotePost interface {
@@ -26,7 +29,20 @@ func NewVotePost(db *sqlx.DB) *VotePostRepository {
 }
 
 func (r *VotePostRepository) LikePost(vote model.Vote) error {
-	panic("not implemented") // TODO: Implement
+	ctx, cancel := context.WithTimeout(context.Background(), viper.GetDuration("database.ctxTimeout"))
+	defer cancel()
+
+	stmt, err := r.db.Preparex(`INSERT INTO likes (user_id, content_type, content_id) VALUES ($1, $2, $3);`)
+	if err != nil {
+		return fmt.Errorf("repo: like post: prepare - %w", err)
+	}
+
+	_, err = stmt.ExecContext(ctx, vote.UserID, vote.ContentType, vote.ContentId)
+	if err != nil {
+		return fmt.Errorf("repo: like post: exec - %w", err)
+	}
+
+	return nil
 }
 
 func (r *VotePostRepository) DislikePost(vote model.Vote) error {
