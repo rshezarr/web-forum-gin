d38539c53d67e8198306f536b1bcd1e127b85e06
rshezarr <rshezarr@gmail.com>
2023-01-05 10:06:30 +0600

    define: vote post repo

--- internal/repository/votePost.go
+++ internal/repository/votePost.go
@@ -3,6 +3,12 @@ package repository
 import "github.com/jmoiron/sqlx"
 
 type VotePost interface {
+	LikePost(userId, contentId int, contentType string) error
+	DislikePost(userId, contentId int, contentType string) error
+	IsLikedPost(userId, contentId int, contentType string) (bool, error)
+	IsDislikedPost(userId, contentId int, contentType string) (bool, error)
+	RemoveLike(userId, contentId int, contentType string) error
+	RemoveDislike(userId, contentId int, contentType string) error
 }
 
 type VotePostRepository struct {
