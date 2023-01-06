55f88d715194d4a117c551a1e08edcd89ed23abc
rshezarr <rshezarr@gmail.com>
2023-01-06 17:22:50 +0600

    init: comment handlers

---
+++ internal/handler/comment.go
@@ -0,0 +1,19 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) createComment(c *gin.Context) {
+
+}
+
+func (h *Handler) getComment(c *gin.Context) {
+
+}
+
+func (h *Handler) updateComment(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteComment(c *gin.Context) {
+
+}
