8d951e58f67fcde747ef1202c3f48505d431e3cd
rshezarr <rshezarr@gmail.com>
2023-01-06 13:51:37 +0600

    fix: middleware

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -10,33 +10,31 @@ import (
 
 const userCtx = "user_id"
 
-func (h *Handler) authMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		authHeader := c.Request.Header.Get("Authorization")
-		if authHeader == "" {
-			newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
-			return
-		}
-
-		headerParts := strings.Split(authHeader, " ")
-		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
-			newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
-			return
-		}
-
-		if len(headerParts[1]) == 0 {
-			newErrorResponse(c, http.StatusUnauthorized, "token is empty")
-			return
-		}
-
-		userID, err := h.service.ParseToken(headerParts[1])
-		if err != nil {
-			newErrorResponse(c, http.StatusUnauthorized, err.Error())
-			return
-		}
-
-		c.Set(userCtx, userID)
+func (h *Handler) authMiddleware(c *gin.Context) {
+	authHeader := c.Request.Header.Get("Authorization")
+	if authHeader == "" {
+		newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
+		return
 	}
+
+	headerParts := strings.Split(authHeader, " ")
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
+		return
+	}
+
+	if len(headerParts[1]) == 0 {
+		newErrorResponse(c, http.StatusUnauthorized, "token is empty")
+		return
+	}
+
+	userID, err := h.service.ParseToken(headerParts[1])
+	if err != nil {
+		newErrorResponse(c, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	c.Set(userCtx, userID)
 }
 
 func getUserId(c *gin.Context) (int, error) {
