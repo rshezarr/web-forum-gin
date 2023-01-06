3fbfd5484a059b1ff1fa52bf545f9f87c0a2a3aa
rshezarr <rshezarr@gmail.com>
2023-01-06 17:28:28 +0600

    add: create comment handler

--- internal/handler/comment.go
+++ internal/handler/comment.go
@@ -1,9 +1,43 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"forum/internal/model"
+	"forum/internal/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) createComment(c *gin.Context) {
+	postId, err := strconv.Atoi(c.Param("post_id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, "invalid id param")
+		return
+	}
 
+	var comment model.Comment
+	if err := c.BindJSON(&comment); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	comment.PostID = postId
+
+	id, err := h.service.Comment.Create(comment)
+	if err != nil {
+		if errors.Is(err, service.ErrInvalidComment) {
+			newErrorResponse(c, http.StatusBadRequest, err.Error())
+			return
+		}
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getComment(c *gin.Context) {
