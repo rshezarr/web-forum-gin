0ee3f43e9b61d5083cf53c3aef8df82d3671a97f
rshezarr <rshezarr@gmail.com>
2023-01-06 13:47:34 +0600

    update: middleware errors

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -14,24 +14,24 @@ func (h *Handler) authMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.Request.Header.Get("Authorization")
 		if authHeader == "" {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			newErrorResponse(c, http.StatusUnauthorized, "empty auth header")
 			return
 		}
 
 		headerParts := strings.Split(authHeader, " ")
 		if len(headerParts) != 2 || headerParts[0] != "Bearer" {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			newErrorResponse(c, http.StatusUnauthorized, "invalid auth header")
 			return
 		}
 
 		if len(headerParts[1]) == 0 {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			newErrorResponse(c, http.StatusUnauthorized, "token is empty")
 			return
 		}
 
 		userID, err := h.service.ParseToken(headerParts[1])
 		if err != nil {
-			c.AbortWithStatus(http.StatusUnauthorized)
+			newErrorResponse(c, http.StatusUnauthorized, err.Error())
 			return
 		}
 
