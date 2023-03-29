3355ea14d87a30fcb4389621540f3b057cb11acb
rshezarr <rshezarr@gmail.com>
2023-03-29 21:14:02 +0600

    init: cache layer

---
+++ internal/cache/redis_cache.go
@@ -0,0 +1,17 @@
+package cache
+
+import "time"
+
+type RedisCache struct {
+	host    string
+	db      int
+	expires time.Duration
+}
+
+func NewRedisCache(host string, db int, exp time.Duration) *RedisCache {
+	return &RedisCache{
+		host:    host,
+		db:      db,
+		expires: exp,
+	}
+}
